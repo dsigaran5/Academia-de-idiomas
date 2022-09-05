6350b05bcf2a9ca7b82197a46d7c523a065ca9f6
hbonilla6 <106295995+hbonilla6@users.noreply.github.com>
2022-09-04 20:40:16 -0600

    delete view Idiomas

--- Academia_De_Idiomas/Controllers/IdiomasController.cs
+++ Academia_De_Idiomas/Controllers/IdiomasController.cs
@@ -4,35 +4,7 @@ namespace Academia_De_Idiomas.Controllers
 {
     public class IdiomasController : Controller
     {
-        public IActionResult Ingles()
-        {
-            return View();
-        }
-        public IActionResult Español()
-        {
-            return View();
-        }
-        public IActionResult Frances()
-        {
-            return View();
-        }
-        public IActionResult Portugues()
-        {
-            return View();
-        }
-        public IActionResult Aleman()
-        {
-            return View();
-        }
-        public IActionResult Italiano()
-        {
-            return View();
-        }
-        public IActionResult Ruso()
-        {
-            return View();
-        }
-        public IActionResult Japones()
+        public IActionResult Idiomas()
         {
             return View();
         }
