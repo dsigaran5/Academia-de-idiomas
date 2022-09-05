9c8350179bef40507fc76794c9f26f759e0ce545
hbonilla6 <106295995+hbonilla6@users.noreply.github.com>
2022-09-04 20:26:37 -0600

    vistas de idiomas

---
+++ Academia_De_Idiomas/Controllers/IdiomasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academia_De_Idiomas.Controllers
+{
+    public class IdiomasController : Controller
+    {
+        public IActionResult Ingles()
+        {
+            return View();
+        }
+        public IActionResult Español()
+        {
+            return View();
+        }
+        public IActionResult Frances()
+        {
+            return View();
+        }
+        public IActionResult Portugues()
+        {
+            return View();
+        }
+        public IActionResult Aleman()
+        {
+            return View();
+        }
+        public IActionResult Italiano()
+        {
+            return View();
+        }
+        public IActionResult Ruso()
+        {
+            return View();
+        }
+        public IActionResult Japones()
+        {
+            return View();
+        }
+    }
+}
