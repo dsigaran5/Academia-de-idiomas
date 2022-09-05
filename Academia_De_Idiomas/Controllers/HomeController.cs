dc9c96f5a71dbda1dd7cddc45a0e5def743f061d
dsigaran5 <dsigaran1946@gmail.com>
2022-09-04 19:47:10 -0600

    first commit

---
+++ Academia_De_Idiomas/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Academia_De_Idiomas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Academia_De_Idiomas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PlanCurso()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Academia_De_Idiomas/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academia_De_Idiomas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
